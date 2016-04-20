5e2b70d5f22f62aec37b3db349a602e7ca19ae18
Ajai <ajainarayanan@gmail.com>
2016-04-19 23:25:21 -0700

    Adds dummy data and 'sort-of' adds a basic structure to the react component

--- src/app.js
+++ src/app.js
@@ -6,15 +6,43 @@ require('bootstrap/dist/css/bootstrap.min.css');
 class DAG extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      nodes: [],
+      connections: []
+    };
+  }
+  componentDidMount() {
+    this.setState({
+      nodes: this.props.nodes,
+      connections: this.props.connections
+    });
+  }
+  onTextChange(event) {
+    this.setState({
+      nodes: JSON.parse(event.target.value),
+      connections: this.state.connections
+    });
   }
   render() {
     return (
-      <h2> Come On! </h2>
+      <div>
+        <h4> Generic DAG </h4>
+        <textarea className="form-control"
+                  defaultValue={JSON.stringify(this.state.nodes)}
+                  onChange={this.onTextChange.bind(this)}
+                  >
+        </textarea>
+        <div>
+          {JSON.stringify(this.state.nodes)}
+        </div>
+      </div>
     );
   }
 }
+let nodes = data.nodes;
+let connections = data.connections;
 
 ReactDOM.render(
-  <DAG></DAG>,
+  <DAG nodes={nodes} connections={connections}></DAG>,
   document.getElementById('app-dag')
 );
---
+++ src/data.js
@@ -0,0 +1,16 @@
+module.exports = {
+  nodes: [
+    {
+      id: 'node1'
+    },
+    {
+      id: 'node2'
+    }
+  ],
+  connections: [
+    {
+      from: 'node1',
+      to: 'node2'
+    }
+  ]
+};
