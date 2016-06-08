2d9e3290c020c0acbfdb214f5411d89a5d67c040
Ajai <ajainarayanan@gmail.com>
2016-06-08 11:02:51 -0700

    Adds FIT-TO-SCREEN action to store to fit large graph into screen

--- src/reducers/layout-reducer.js
+++ src/reducers/layout-reducer.js
@@ -1,12 +1,11 @@
 var dagre = require('dagre');
 
-var getLayout = (separation, {nodes, connections}) => {
-  var rankSeparation = separation || 200;
-
+var getLayout = (separation = 10, {nodes, connections}) => {
+  var rankSeparation = separation;
   var graph = new dagre.graphlib.Graph();
   graph.setGraph({
-    nodesep: 90,
-    ranksep: rankSeparation,
+    nodesep: 10,
+    ranksep: 100,
     rankdir: 'LR',
     marginx: 0,
     marginy: 0
@@ -15,7 +14,7 @@ var getLayout = (separation, {nodes, connections}) => {
 
   nodes.forEach(function (node) {
     var id = node.id || node.name;
-    graph.setNode(id, { label: node.label, width: 100, height: 100 });
+    graph.setNode(id, { label: node.label, width: 105, height: 66 });
   });
 
   connections.forEach(function (connection) {
@@ -27,10 +26,9 @@ var getLayout = (separation, {nodes, connections}) => {
 };
 
 export function graphLayout(state = [], action = {}) {
-
   switch(action.type) {
     case 'CLEANUP-GRAPH':
-      let layout = getLayout(200, action.payload);
+      let layout = getLayout(100, action.payload);
       return state.map( node => {
         node.style = {
           top: layout._nodes[node.id].y + 'px',
@@ -42,3 +40,18 @@ export function graphLayout(state = [], action = {}) {
       return state;
   }
 }
+
+export function graph(state = { scale: 1}, action = {}) {
+  switch(action.type) {
+    case 'FIT-TO-SCREEN':
+      let {nodes, connections, parentDimension} = action.payload;
+      let layout = getLayout(100, {nodes, connections});
+      let {width, height} = parentDimension;
+      let widthScale = (width - 100) / layout.graph().width;
+      let heightScale = (height - 100) / layout.graph().height;
+      let scale = Math.min(widthScale, heightScale);
+      return Object.assign({}, state, {scale: scale > 1 ? 1 : scale});
+    default:
+      return state;
+  }
+};
