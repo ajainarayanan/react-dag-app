8a319488470ac9b5d9d21f81346c510ab50ffa66
Ajai <ajainarayanan@gmail.com>
2016-06-16 02:16:31 -0700

    Removes happypack module reference from webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ var webpack = require('webpack');
 var path = require('path');
 var LiveReloadPlugin = require('webpack-livereload-plugin');
 var LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
-var HappyPack = require('happypack');
 
 module.exports = {
   context: __dirname + '/src',
