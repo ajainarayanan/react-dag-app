d4274fac5de6179d9bf3c2edb060369ba38705cc
Ajai <ajainarayanan@gmail.com>
2016-06-16 01:15:59 -0700

    Minor modifications to package.json & webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var webpack = require('webpack');
 var path = require('path');
 var LiveReloadPlugin = require('webpack-livereload-plugin');
 var LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+var HappyPack = require('happypack');
 
 module.exports = {
   context: __dirname + '/src',
@@ -11,20 +12,21 @@ module.exports = {
     'html': './index.html'
   },
   module: {
-    preLoaders: [
-      // {
-      //   test: /\.js$/,
-      //   exclude: /node_modules/,
-      //   loader: "jshint-loader"
-      // }
-    ],
     loaders: [
       {
+        test: /node_module\/dagre\/dist\/dagre.core.js/,
+        loaders: [
+          'imports?this=>window',
+          'script'
+        ]
+      },
+      {
         test: /\.html$/,
+        exclude: /node_modules/,
         loader: 'file?name=[name].[ext]'
       },
       {
-        test:  /\.(ttf|eot|svg|woff|woff(2))(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        test: /\.(ttf|eot|svg|woff|woff(2))(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: "file-loader"
       },
       {
@@ -44,30 +46,30 @@ module.exports = {
       }
     ]
   },
-  jshint: {
-      camelcase: true,
-      emitErrors: false,
-      failOnHint: false,
-      reporter: function(errors) { }
-  },
   output: {
     filename: './[name].js',
     path: __dirname + '/dist'
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"development"'
+      'process.env':{
+        'NODE_ENV': JSON.stringify("production"),
+        '__DEVTOOLS__': false
+      },
     }),
     new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.js", Infinity),
     new LodashModuleReplacementPlugin,
     new webpack.optimize.DedupePlugin(),
     new LiveReloadPlugin(),
-    // new webpack.optimize.UglifyJsPlugin({
-    //   compress: {
-    //     warnings: false,
-    //     drop_console: false,
-    //     dead_code: true
-    //   }
-    // })
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: true,
+        drop_console: false,
+        dead_code: true
+      },
+      output: {
+        comments: false
+      }
+    })
   ]
 };
